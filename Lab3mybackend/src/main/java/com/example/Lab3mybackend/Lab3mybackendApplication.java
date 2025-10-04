7f30e40a517e78c45bec1b2f11e960a3b7c98745
davidmoi2135 <caocaonguyentrinh210305@gmail.com>
2025-10-04 10:48:48 +0700

    First commit

---
+++ Lab3mybackend/src/main/java/com/example/Lab3mybackend/Lab3mybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab3mybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3mybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3mybackendApplication.class, args);
+	}
+
+}
---
+++ Lab3mybackend/src/test/java/com/example/Lab3mybackend/Lab3mybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab3mybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3mybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
